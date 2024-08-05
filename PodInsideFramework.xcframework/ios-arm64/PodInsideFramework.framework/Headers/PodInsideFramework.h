70c640eddf3d267de6ba65902d7014bc93f2af6f
architdhupar <archit.dhupar@gmail.com>
2024-08-06 00:04:44 +0200

    Added MyFramework.framework

---
+++ PodInsideFramework.xcframework/ios-arm64/PodInsideFramework.framework/Headers/PodInsideFramework.h
@@ -0,0 +1,18 @@
+//
+//  PodInsideFramework.h
+//  PodInsideFramework
+//
+//  Created by Archit Dhupar on 2024-08-04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodInsideFramework.
+FOUNDATION_EXPORT double PodInsideFrameworkVersionNumber;
+
+//! Project version string for PodInsideFramework.
+FOUNDATION_EXPORT const unsigned char PodInsideFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodInsideFramework/PublicHeader.h>
+
+
